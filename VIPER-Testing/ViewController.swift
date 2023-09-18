ffa25f0793530063622223b32f27220b76b8454b
Leonardo Wijaya <leonardo.wijaya003@binus.ac.id>
2023-09-18 16:30:29 +0700

    Initial Commit

---
+++ VIPER-Testing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPER-Testing
+//
+//  Created by Leonardo Wijaya on 18/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
